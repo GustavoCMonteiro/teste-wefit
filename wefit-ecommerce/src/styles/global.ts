f010e15a2b526a0d0b84f2311682140d34ece769
Gustavo Monteiro <gustavo.monteiro@intelitrader.com.br>
2023-08-29 18:08:32 -0300

    Adiciona arquivos, e faz modificacoes globais

---
+++ wefit-ecommerce/src/styles/global.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+import theme from "./theme";
+
+export default createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+        font-family: 'Open Sans', sans-serif; 
+    }
+    #root{
+        margin: 0 auto;
+    }
+    body {
+        background-color: ${theme.bg.default};
+    }
+ `;
---
+++ wefit-ecommerce/src/styles/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  bg: {
+    default: "#2F2E41",
+    primary: "#ffffff",
+    secondary: "#009EDD",
+    tertiary: "#039B00",
+  },
+  text: {
+    default: "#ffffff",
+    primary: "#333333",
+    secondary: "#999999",
+    tertiary: "#2F2E41",
+  },
+};
+
+export default theme;
